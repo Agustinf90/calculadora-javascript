6f298a7a6a1dfe3e69f4594bb0458e5743d6cc32
Agustinf90 <agustinf_tw@hotmail.com>
2022-06-09 14:17:55 -0300

    ultima

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ Vue.component('display', {
     template: `
     <div style="display: grid; grid-area: 1 / 1 / auto / 6; background-color: black; margin-left: 25px;
     margin-right: 25px; color: white; margin-bottom: 15px; padding: 12px; text-align: end; font-size: 26px;"> {{ valor }}
-
     </div>
     `
-})
+});
 
 Vue.component('botonera', {
     data: function () {
@@ -21,54 +20,50 @@ Vue.component('botonera', {
     methods: {
         action(tecla) {
                 if (!isNaN(tecla) || tecla === '.') {    
-                    this.valor_actual_display += tecla + ''
+                    this.valor_actual_display += tecla + '';
                 }   
                 if(['+','-','*','/'].includes(tecla)) {
-                    let numerico = this.valor_actual_display
-                    numerico = Number(numerico)
-                    this.valor_actual_display = numerico
-                    this.valor_anterior_display = this.valor_actual_display
-                    this.operador = tecla
-                    this.valor_actual_display = ' '
+                    let numerico = this.valor_actual_display;
+                    numerico = Number(numerico);
+                    this.valor_actual_display = numerico;
+                    this.valor_anterior_display = this.valor_actual_display;
+                    this.operador = tecla;
+                    this.valor_actual_display = ' ';
                 }
                 if(tecla === 'calc') {
-                    let numerico = this.valor_actual_display
-                    numerico = Number(numerico)
-                    this.valor_actual_display = numerico
+                    let numerico = this.valor_actual_display;
+                    numerico = Number(numerico);
+                    this.valor_actual_display = numerico;
                         this.valor_actual_display = eval(
                         this.valor_anterior_display + this.operador + this.valor_actual_display  
-                    )
+                    );
                     if(this.valor_actual_display === Infinity)
                     {
-                        alert("error")
-                    this.valor_actual_display = ' '
-                    }
-                    
+                        alert("error");
+                    this.valor_actual_display = ' ';
+                    };
                     if(this.valor_actual_display % 1 != 0) {
-                    let numero = this.valor_actual_display
-                    let numeroEntero = numero.toFixed(2)
-                    this.valor_actual_display = numeroEntero
-                    }
-                    this.valor_anterior_display = ' '
-                    this.operador = ' '
-                }
+                    let numero = this.valor_actual_display;
+                    let numeroEntero = numero.toFixed(2);
+                    this.valor_actual_display = numeroEntero;
+                    };
+                    this.valor_anterior_display = ' ';
+                    this.operador = ' ';
+                };
                 if (tecla === 'clear') {
-                    this.valor_actual_display = ''
-               } 
+                    this.valor_actual_display = '';
+               }; 
           }
       },
     template: `
-    <div>
-    
-    <div style="display: grid; grid-template-columns: repeat(5, 100px); grid-template-rows: repeat(4, 60px);
+    <div><div style="display: grid; grid-template-columns: repeat(5, 100px); grid-template-rows: repeat(4, 60px);
     background-color: grey; margin: auto; padding: 25px; width: 500px; border-radius: 10px;">
     <display :valor="valor_actual_display"></display>
     <div style="display: flex; justify-content: center;" v-for="k in contador" :key="k">
     <button style="width: 55px; display: flex; justify-content: center; font-size: 22px; height: 30px;
-    border-radius: 10px;"v-on:click="action(k)">{{ k }}</button> </div>
-    </div>
+    border-radius: 10px;"v-on:click="action(k)">{{ k }}</button> </div></div>
     </div>`
-})
+});
 
 new Vue({
     el: '#calculadora',
