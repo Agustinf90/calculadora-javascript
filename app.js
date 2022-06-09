04bb5046f8555dc842e47e259d437b173c12b876
Agustinf90 <agustinf_tw@hotmail.com>
2022-06-09 12:55:39 -0300

    calc

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ Vue.component('botonera', {
         return {
          valor_actual_display: ' ',
          valor_anterior_display: ' ',
-          contador: ["*","/","+","-",9,8,7,6,5,4,3,2,1,0,".","clear", "calc"],
+          contador: [9,8,7,"*",6,5,4,"/",3,2,1,"+",0,".","clear","-","calc",],
           operador: ' '
         }
       },
@@ -31,9 +31,18 @@ Vue.component('botonera', {
                     this.valor_actual_display = ' '
                 }
                 if(tecla === 'calc') {
+                    let numerico = this.valor_actual_display
+                    numerico = Number(numerico)
+                    this.valor_actual_display = numerico
                         this.valor_actual_display = eval(
                         this.valor_anterior_display + this.operador + this.valor_actual_display  
                     )
+                    if(this.valor_actual_display === Infinity)
+                    {
+                        alert("error")
+                    this.valor_actual_display = ' '
+                    }
+                    
                     if(this.valor_actual_display % 1 != 0) {
                     let numero = this.valor_actual_display
                     let numeroEntero = numero.toFixed(2)
@@ -48,11 +57,13 @@ Vue.component('botonera', {
           }
       },
     template: `
-    <div style="display: grid; grid-template-columns: repeat(5, 80px); grid-template-rows: 60px repeat(4, 60px);
+    <div style="display: grid; grid-template-columns: repeat(4, 100px);
+    grid-template-rows: 60px repeat(5, 60px);
     background-color: grey; margin: auto; padding: 25px; width: 400px; border-radius: 10px;">
     <div style="display: flex;
     justify-content: center;" v-for="k in contador" :key="k">
-    <button style="width: 50px; height: 30px; padding: 10px; border-radius: 10px;"v-on:click="action(k)">{{ k }}</button>
+    <button style="width: 55px; display: flex; justify-content: center; font-size: 22px; height: 30px;
+    border-radius: 10px;"v-on:click="action(k)">{{ k }}</button>
     </div>
     <display :valor="valor_actual_display"></display>
     </div>`
